b27ec3b5a1596e601be70097a06165a8a121f719
Willie Rogers <wjrogers@mail.nih.gov>
2014-02-27 11:02:29 -0500

    Added aapp and carb to semantic types used by MetaMap method.
    Added "Amino Acid, Peptide, or Protein" and "Carbohydrate" semantic
    types to list of concepts kept.
    
    Commented out "Chemical (chem) and "Chemical Viewed Functionally" (chvf).

--- src/chem/semtypes.clj
+++ src/chem/semtypes.clj
@@ -3,18 +3,22 @@
 ;; chemical UMLS semantic types 
 (def chemical-semantic-type-list 
   [
-   "inch"                               ;inorganic chemical
-   "orch"                               ;organic chemical
-   "chem"                               ;chemical
-   "chvf"                               ;chemical viewed functionally
+   "aapp"                               ;amino acid, peptide, or protein
+   "carb"                               ;carbohydrate
+;; "chem"                               ;chemical
+;; "chvf"                               ;chemical viewed functionally
    "chvs"                               ;chemical viewed structurally
    "elii"                               ;element, ion, or isotope
+   "inch"                               ;inorganic chemical
+   "opco"                               ;organophosphorus compound
+   "orch"                               ;organic chemical
    ])
 
 ;; possible chemicals
 ;; "bacs" biologically active substance
-;; "phsu"  pharmalogic substance
+;; "phsu" pharmalogic substance
 ;; "sbst" substance
 ;; "hops" hazardous or poisonous substance
+;; "enzy" enzyme
 
 (def chemical-semantic-type-set (set chemical-semantic-type-list))
