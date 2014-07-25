1b3561ea97ed59ccaf461fa0496f6c6bbc8be767
Willie Rogers <wjrogers@mail.nih.gov>
2014-07-25 17:38:08 -0400

    Updated Chemical Partial match dictionaries.

--- src/chem/dictionaries.clj
+++ src/chem/dictionaries.clj
@@ -3,52 +3,103 @@
 ;; A really simple example of an annotation engine; engine looks for
 ;; partial chemical dictionary matches.
 
-(def fragment-dictionary
-  #{ 
+;; multipliers ,
+;; table 11(IUPAC 1996)
+;; (http://en.wikipedia.org/wiki/IUPAC_numerical_multiplier)
+(def multipliers
+  #{"deca"
+    "dodeca"
+    "hept"
+    "hepta"
+    "hex"
+    "hexa"
+    "nona"
+    "octa"
+    "pent"
+    "penta"
+    "tetra"
+    "tetrodeca"
+    "trideca"
+    "undeca"
+    ;; "di"
+    ;; "mono"
+    })
+
+;; element names
+(def elements
+  #{
+   "carbon"
+   "deuterium"
+   "helium"
+   "hydrogen"
+   "iron"
+   "nitrogen"
+   "oxygen"
+   })
+
+;; trival names
+(def trival-names
+  #{
+    "benzene"
+    "butene"
+    "fluorine"
+    "furan"
+    "lerythrose"
+    "pyridine"
+    "ribose"
+    "thiophene"
+    "threose"
+    })    
+
+(def fragment-suffix-dictionary
+  #{
     "dioxide"
-    "ethyl"
     "hexose"
     "iose"
     "trioxide"
     "saccharide"
-    "methi"
-    "methane"
-    "thane"
-    "hexo"
+    "actate"
     "kinase"
     "drogen"
     "ocaine"
     "idine"
+    "crylate"
+    "citric"
+    })
+
+;; sugar lexicon entries
+(def sugar-lexicon #{ "deoxy" "amino" "thio"})
+
+;; nonsugar lexicon entries
+(def non-sugar-lexicon #{})
+
+;; class name
+(def class-names #{"alkene" "alcohol" "acid"})
+
+
+;; trival ring names
+(def trival-ring-names #{"oxirose" "oxetose" "furanose"})
+
+(def fragment-dictionary
+  (clojure.set/union multipliers elements trival-names 
+                     sugar-lexicon non-sugar-lexicon 
+                     class-names trival-ring-names
+                     fragment-suffix-dictionary
+                     
+  #{ 
+    "ethyl"
+    "methi"
+    "methane"
+    "thane"
+    "hexo"
     "nitr"
     "phenyl"
     "acetyl"
     "propyl"
     "oxyl"
-    "trate"
 
-;; multipliers ,
-;; table 11(IUPAC 1996)
-;; (http://en.wikipedia.org/wiki/IUPAC_numerical_multiplier)
-    "penta"
-    "pent"
-    "hexa"
-    "hex"
-    "hepta"
-    "hept"
-    "mono"
-;;  "di"
-    "tri"
-    "tetra"
-    "octa"
-    "nona"
-    "deca"
-    "undeca"
-    "dodeca"
-    "trideca"
-    "tetrodeca"
-
-
-;; nonsugar lexicon entries
+    ;; "trate"
+    "glycol"
 
 ;; a-terms/replacement prefixes
     "oxa"
@@ -59,45 +110,13 @@
 ;;  "ox"
 ;;  "phosph"
 ;;  "ars"
-   
-;; trival names
-   "benzene"
-   "thiophene"
-   "furan"
-   "butene"
-   "ribose"
-
-;; class name
-   "alkene"
-   "alcohol"
-   "acid"
-
-;; element names
-   "oxygen"
-   "hydrogen"
-   "carbon"
-   "nitrogen"
-   "helium"
-
-;; sugar lexicon entries
-   "deoxy"
-   "amino"
-   "thio"
-
-;; trival ring names
-   "oxirose"
-   "oxetose"
-   "furanose"
 
 
-
-   
-
-
-    })
+    }))
 
 
 (def prefix-dictionary 
+  (clojure.set/union multipliers elements trival-names
   #{
     ;; cyclo - prefix
     "cyclo"
@@ -111,40 +130,50 @@
     "erythro"
     "threo"
     
-    ;; trival names
-    "lerythrose"
-    "threose"
-    "ribose"
+    }))
 
-    })
+;; functional group suffixes ,
+;; table 5(IUPAC 1996)
+(def functional-group-suffixes
+  #{"ol" "one" "aldehyde"})
 
-(def suffix-dictionary
-  #{
-    ;; functional group suffixes ,
-    ;; table 5(IUPAC 1996)
-    ;; "ol"
-    ;; "one"
-    "aldehyde"
-    
-    ;; saturation parent suffix
-    ;; "an"
-    ;; "ane"
-    
+;; saturation parent suffix
+;; "an"
+(def saturation-parent-suffix #{"ane"} )
+
     ;; unsaturation parent suffix
-    "ene"
-    "yne"
-    
-    ;; ring stem suffix
-    "anose"
-    "anos"
+(def unsaturation-parent-suffix
+  #{"ene" "yne"})
 
-    ;; parent structure stem suffixes
+;; ring stem suffix
+(def ring-stem-suffix #{"anose" "anos"})
+
+;; parent structure stem suffixes
+(def parent-structure-stem-suffixes
+  #{
     ;; "ose"
     ;; "os"
     "aldose"
     "ulose"
     "ketose"
 
-    
+    "lidine"
+    "iminium"})
 
-    })
+(def suffix-dictionary
+  (clojure.set/union functional-group-suffixes
+                     saturation-parent-suffix
+                     unsaturation-parent-suffix
+                     ring-stem-suffix
+                     parent-structure-stem-suffixes
+                     fragment-suffix-dictionary
+  #{
+     ;; -ine is a suffix used in chemistry to denote two kinds of
+;; -substance. The first is a chemically basic and alkaloidal
+;; -substance
+    "ine"
+
+    }))
+
+(defn corpus-targetset [records]
+  (set (flatten (map #(into [] (:chemdner-gold-standard %)) records))))
