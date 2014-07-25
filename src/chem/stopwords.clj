a75d068b1df550230e423e2e701da3fa20a77a22
Willie Rogers <wjrogers@mail.nih.gov>
2014-07-25 17:29:03 -0400

    Added more terms to stopwords set.

--- src/chem/stopwords.clj
+++ src/chem/stopwords.clj
@@ -4,22 +4,72 @@
 
 (def stopwords
   #{
+    "about"
+    "acid"
+    "acidic"
     "activation"
+    "alkaloid"
+    "all"
+    "alone"
+    "and"
+    "are"
+    "as"
+    "but"
     "compound"
     "compounds"
+    "concluded"
+    "contains"
+    "control"
+    "deals"
     "demethylation"
+    "determine"
+    "disease"
+    "distant"
     "enzyme"
     "enzymes"
+    "for"
+    "from"
     "hypermethylated"
     "hypomethylated"
+    "illustrate"
     "induced"
+    "intrinsic"
+    "its"
+    "liquid"
+    "mediated"
+    "met"
     "methylation"
     "one"
     "optimal"
+    "other"
+    "region"
+    "target"
+    "that"
+    "the"
     "unmethylated"
+    "were"
+    "where"
+    "with"
     })
+
 
 (def component-stopwords
   #{
     "induced"
+    "demethylation"
+    })
+
+;; Notes
+;;
+;; Proteins are not considered chemicals for the purposes of
+;; BioCreative.
+;;
+;;  Terms ending in "rase", "ribose", and possibly others
+;; should be removed from consideration.
+;; (this should be evaluated, are there exceptions?)
+;;
+(def suffix-stopwords
+  #{
+    "rase"
+    "ribose"
     })
