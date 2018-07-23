f0bc007ea32ad6a37b2d8ca8a42ace4ca8742daf
Willie Rogers <wjrogers@nih.gov>
2018-07-23 12:28:01 -0400

    Added ChemDNER patents collection to data and paths modules.

--- src/chem/chemdner_data.clj
+++ src/chem/chemdner_data.clj
@@ -4,13 +4,17 @@
 
 ;; Load CHEMDNER training, testing, and development corpora.
 
-(defn load-chemdner-training-data []
+(defn load-chemdner-data []
   ^{:doc "load chemdner training data into map."}
   (let [training-records (chemdner-tools/load-chemdner-abstracts paths/training-text)
-        chemdner-training-cdi-gold (chem.chemdner-tools/load-training-document-entity-gold-standard 
+        chemdner-training-cdi-gold (chemdner-tools/load-training-document-entity-gold-standard 
                                     paths/training-entities)
-        chemdner-development-cdi-gold (chem.chemdner-tools/load-training-document-entity-gold-standard
+        chemdner-development-cdi-gold (chemdner-tools/load-training-document-entity-gold-standard
                                        paths/development-entities)
+        chemdner-training-cem-gold (chemdner-tools/load-training-document-extents-gold-standard 
+                                    paths/training-extents)
+        chemdner-development-cem-gold (chemdner-tools/load-training-document-extents-gold-standard
+                                       paths/development-extents)
         development-records (chemdner-tools/load-chemdner-abstracts paths/development-text)
         test-records (chemdner-tools/load-chemdner-abstracts paths/test-text)
         
@@ -20,11 +24,24 @@
      :training-filename paths/training-text
      :training-records training-records
      :chemdner-training-cdi-gold chemdner-training-cdi-gold
+     :chemdner-training-cem-gold chemdner-training-cem-gold
      :development-filename paths/development-text
      :development-records development-records
      :chemdner-development-cdi-gold chemdner-development-cdi-gold
+     :chemdner-development-cem-gold chemdner-development-cem-gold
      :training-record-map (into {} (map #(vec (list (:docid %) %)) training-records))
      :test-records test-records
      :training-annotations training-annotations
      :development-annotations development-annotations
+     )))
+
+(defn load-patent-chemdner-data []
+  ^{:doc "load chemdner training data into map."}
+  (let [training-records (chemdner-tools/load-chemdner-abstracts paths/patent-training-text)
+        development-records (chemdner-tools/load-chemdner-abstracts paths/patent-development-text)]
+    (hash-map
+     :training-filename paths/patent-training-text
+     :training-records training-records
+     :development-filename paths/patent-development-text
+     :development-records development-records
      )))
--- src/chem/chemdner_paths.clj
+++ src/chem/chemdner_paths.clj
@@ -1,7 +1,8 @@
 (ns chem.chemdner-paths)
 
-;; (def biocreative-root "/nfsvol/nlsaux16/II_Group_WorkArea/Lan/projects/BioCreative/2013")
-(def biocreative-root "/net/lhcdevfiler/vol/cgsb5/ind/II_Group_WorkArea/Lan/projects/BioCreative/2013")
+(def workarea "/net/lhcdevfiler/vol/cgsb5/ind/II_Group_WorkArea")
+
+(def biocreative-root (str workarea "/Lan/projects/BioCreative/2013"))
 
 (def training-dir (str biocreative-root "/CHEMDNER_TRAIN_V01"))
 (def training-text (str training-dir "/chemdner_abs_training.txt"))
@@ -22,3 +23,18 @@
 (def test-dir (str biocreative-root "/CHEMDNER_TEST_V01"))
 (def test-text (str test-dir "/chemdner_abs_test.txt"))
 
+;; CHEMDNER Patents Collection
+;; /net/lhcdevfiler/vol/cgsb5/ind/II_Group_WorkArea/wjrogers/Collections/BioCreativeV/CHEMDNER
+(def patent-biocreative-root (str workarea "/wjrogers/Collections/BioCreativeV/CHEMDNER"))
+
+(def patent-training-dir (str patent-biocreative-root "/cemp_training_set"))
+(def patent-training-text (str patent-training-dir "/chemdner_patents_train_text.txt"))
+(def patent-training-eval (str patent-training-dir "/chemdner_cemp_gold_standard_train.tsv"))
+(def patent-training-extents (str patent-training-dir "/chemdner_cemp_gold_standard_train_eval.tsv"))
+(def patent-training-annotations (str patent-training-dir "/chemdner_cemp_gold_standard_train.tsv"))
+
+(def patent-development-dir (str patent-biocreative-root "/cemp_development_set_v02"))
+(def patent-development-text (str patent-development-dir "/chemdner_patents_development_text.txt"))
+(def patent-development-eval (str patent-development-dir "/chemdner_cemp_gold_standard_development_eval_v02.tsv"))
+(def patent-development-extents (str patent-development-dir "/chemdner_cemp_gold_standard_development_eval_v02.tsv"))
+(def patent-development-annotations (str patent-development-dir "/chemdner_cemp_gold_standard_development_v02.tsv"))
