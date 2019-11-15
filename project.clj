b9509521fc89c23804dc19b6acc570c2e8ddac77
Willie Rogers <wjrogers@nih.gov>
2019-11-15 14:39:49 -0500

    moved webapp to chem.appserver, updated project file
    Compojure webapp now resides in chem.appserver, made numerous updates
    to project.clj to support new features.

--- project.clj
+++ project.clj
@@ -1,73 +1,71 @@
 (defproject chem "0.1.0-SNAPSHOT"
   :description "Chemical Term Detection"
-  :url "http://ii.nlm.nih.gov/"
+  :url "https://ii.nlm.nih.gov/"
   :license {:name "Public Domain"
-            :url "http://metamap.nlm.nih.gov/MMTnCs.shtml"}
+            :url "https://metamap.nlm.nih.gov/MMTnCs.shtml"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.xml "0.0.8"]
-                 [org.clojure/data.zip "0.1.2"]
+                 [org.clojure/data.zip "0.1.3"]
                  [org.clojure/data.json "0.2.6"]
+                 [org.clojure/tools.cli "0.4.2"]
+                 [org.clojure/tools.logging "0.5.0"]
+                 [org.clojure/math.numeric-tower "0.0.2"]
                  [cc.mallet/mallet "2.0.7"]
                  [clojure-opennlp "0.3.2"] ;; uses Opennlp 1.5.3
-                 [se.sics/prologbeans "4.2.1"]
-                 [ns-tracker "0.3.1"]
-                 [congomongo "0.4.1"]
-                 [com.novemberain/monger "1.7.0"]
+                 [se.sics/prologbeans "4.3.2"]
                  [org.apache.lucene/lucene-core "4.10.0"]
                  [org.apache.lucene/lucene-queryparser "4.10.0"]
                  [org.apache.lucene/lucene-queries "4.10.0"]
                  [org.apache.lucene/lucene-analyzers-common "4.10.0"]
                  [org.apache.lucene/lucene-highlighter "4.10.0"]
-                 [instaparse "1.4.3"]
+                 [instaparse "1.4.10"]
                  [log4j/log4j "1.2.17"]
                  [rhizome "0.1.9"]
+                 [reduce-fsm "0.1.4"]
                  [javax.servlet/servlet-api "2.5"]
-                 [compojure "1.6.0" :exclusions [instaparse]]
+                 [compojure "1.5.1" :exclusions [instaparse]]
                  [hiccup "1.0.5"]
                  [clj-http "2.3.0" :exclusions [commons-logging
                                                 org.clojure/tools.reader
                                                 com.fasterxml.jackson.core/jackson-core]]
-                 [clj-sockets "0.1.0"]
-                 [com.gearswithingears/async-sockets "0.1.0"]
-                 [ring/ring-headers "0.3.0"]
-                 [ring/ring-servlet "1.6.1"]
-                 [ring/ring-jetty-adapter "1.6.1"]
-                 [ring/ring-json "0.4.0"]
-                 [ring.middleware.logger "0.5.0"]
-                 [uk.ac.cam.ch.opsin/opsin-core "1.5.0"]
-                 [bioc "1.0.1"]
+                 [ring/ring-core "1.7.1"]
+                 [ring/ring-jetty-adapter "1.7.1"]
+                 [uk.ac.cam.ch.opsin/opsin-core "2.4.0"]
+                 [me.raynes/fs "1.4.6"]
+                 [clj-glob "1.0.0"]
                  [gov.nih.nlm.ctx/ctx "1.0-SNAPSHOT"]
                  [gov.nih.nlm.nlp/meta-stacking "1.0-SNAPSHOT"]
                  [gov.nih.nlm.nls/stanford-ner-extensions "1.0-SNAPSHOT"]
                  [hashtrie "0.1.0-SNAPSHOT"]
+                 [skr "0.1.2-SNAPSHOT"]
+                 [ii/utils "0.1.1-SNAPSHOT"]
+                 [bioc "1.0.1"]
                  [gov.nih.nlm.nls/metamap-api "2.0"]
-                 [metamap-api "1.0-SNAPSHOT"]
-                 [skr "0.1.0-SNAPSHOT"]
-                 [utils "0.1.0-SNAPSHOT"]
-                 [gov.nih.nlm.nls/mallet-example "1.0-SNAPSHOT"]
-                 [lexicalsystems/lex-access "2012"]
+                 ;; [metamap-api "2.0"]
+                 ;; [gov.nih.nlm.nls/mallet-example "1.0-SNAPSHOT"]
+                 [gov.nih.nlm.nls.lexaccess/lexaccess-dist "2013"]
                  [javax.servlet/servlet-api "2.5"]
                  [clj-diff "1.0.0-SNAPSHOT"]
-                 [irutils "2.0"]
-                 [reduce-fsm "0.1.4"]]
-  :plugins [[lein-ring "0.12.0" :exclusions [org.clojure/clojure]]]
-  :repositories [;; ["java.net" "http://download.java.net/maven/2"]
-                 ;; ["ucc-repo" "http://maven.ch.cam.ac.uk/m2repo"]
-                 ["ii-repo" "https://metamap.nlm.nih.gov/maven2"]]
+                 [irutils "2.0-SNAPSHOT"]
+                 [dosage-info "0.1.0-SNAPSHOT"]
+                 [standoff "0.1.0-SNAPSHOT"]
+                 [com.novemberain/monger "3.1.0"]
+                 ;; [uncomplicate/neanderthal "0.24.0"]  ; be sure that The Intel Math Kernel Library is in LD_LIBRARY_PATH
+                 [congomongo "1.1.0"]]
+  :plugins [[lein-ring "0.8.12" :exclusions [org.clojure/clojure]]
+            [lein-jupyter "0.1.16":exclusions [org.clojure/clojure
+                                               cider/cider-nrepl]]]
+  :repositories [["metamap" "https://metamap.nlm.nih.gov/maven2"]
+                 ;; ["java.net" "https://download.java.net/maven/2"]
+                 ;; ["ucc-repo" "https://maven.ch.cam.ac.uk/m2repo"]
+                 ]
   :ring {:init chem.backend/init
          :handler chem.appserver/app}
   :profiles {:dev
              {:dependencies
-              [[org.clojure/java.classpath "0.2.2"]
-               [org.clojure/math.numeric-tower "0.0.2"]]}}
+              [[org.clojure/java.classpath "0.2.2"]]}}
   :marginalia {:javascript ["mathjax/MathJax.js"]}
   :uberjar {:aot :all}
-  :aot [chem.core
-        chem.socketserver
-        chem.irutils
-        chem.irutils-normchem
-        chem.process
-        chem.span-utils]
-  :main chem.core
-  :pedantic? true)
+  :aot [chem.core]
+  :main chem.core)
 
---
+++ src/chem/appserver.clj
@@ -0,0 +1,123 @@
+(ns chem.appserver
+  (:require [compojure.core :refer :all]
+            [hiccup.core :refer :all]
+            [hiccup.page :refer :all]
+            [hiccup.util :refer :all]
+            [clojure.tools.logging :as log]
+            [hiccup.middleware :refer [wrap-base-url]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.multipart-params :refer :all]
+            [chem.html-views :refer :all]
+            [clojure.string :as string]
+            [compojure.route :as route :refer [resources]]
+            [chem.json-views :as json-views]
+            [chem.cdi-views :as cdi-views]
+            [chem.piped-views :as piped-views]
+            [chem.process :as process]
+            [chem.utils :as utils]
+            [chem.annotations :as annot]
+            [chem.chemdner-tools :as chemdner-tools]
+            [chem.pipeline :as pipeline]
+                        [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.content-type :refer [wrap-content-type]]
+            [ring.middleware.nested-params :refer [wrap-nested-params]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.middleware.multipart-params :refer [wrap-multipart-params]])
+  (:import (java.net URLDecoder URLEncoder))
+  (:gen-class))
+
+(defn print-request
+  "Print request to standard output."
+  [request]
+  (.println System/out "============================================")
+  (.println System/out (str "request keys: " (keys request)))
+  (dorun 
+   (map (fn [[key value]]
+            (.println System/out (format "(request %s) -> %s" key value)))
+        request)))
+
+(defn log-request
+  "Print request to log."
+  [request]
+  (log/info "============================================")
+  (log/info (str "request keys: " (keys request)))
+  (dorun 
+   (map (fn [[key value]]
+            (.println System/out (format "(request %s) -> %s" key value)))
+        request)))
+
+(defn wrap-nop
+  [handler]
+  (fn [request]
+    (log-request request)
+    (handler request)))
+
+(defroutes viewer-routes
+
+  (GET "/" [] (fn [req] (frontpage req)))
+
+  (GET "/adhoc/" [] (fn [req] (adhoc-page req)))
+
+  (GET "/chemdner/" [] 
+    (fn [req]
+      (println (str "/chemdner/ " (prn-str (:params req))))
+      (cond (empty? (:params req)) (chemdner-page req)
+            (contains? (:params req) "docid") (chemdner-document-page req (get (:params req) "docid")))))
+
+  (GET "/altpubmed/" [] 
+    (fn [req]
+      (println (str "/altpubmed/ " (prn-str (:params req))))
+      (cond (empty? (:params req)) (pubmed-page req)
+            (contains? (:params req) "pmid")
+            (pubmed-document-page req
+                                  (get (:params req) "pmid")
+                                  (if (contains? (:params req) "engine")
+                                    (get (:params req) "engine")
+                                    "combine3")))))
+
+  (GET "/pubmed/" [] (pubmed-entry-page))
+  (GET "/pubmed/:pmid/" [pmid] 
+    (println (str "/pubmed/ " pmid))
+    (json-views/pubmed-output pmid))
+
+  (GET "/pubmedjson/" [] (pubmed-entry-page))
+  (GET "/pubmedjson/:pmid/" [pmid] 
+    (println (str "/pubmedjson/ " pmid))
+    (json-views/pubmed-output pmid))
+
+  (GET "/pubmedjson/:pmid/:engine/" [pmid engine] 
+    (json-views/pubmed-output pmid engine))
+
+  (GET "/pubmedcdi/:pmid/:engine/" [pmid engine] 
+    (cdi-views/pubmed-output pmid engine))
+
+  (POST "/adhoc/json/" [document engine] 
+    (.println System/out (format "document: %s, engine: %s" document engine))
+    (json-views/adhoc-output document (if (nil? engine)
+                                        "combine5"
+                                        engine)))
+
+  (POST "/adhoc/piped/" [document engine] 
+    (.println System/out (format "document: %s, engine: %s" document engine))
+    (piped-views/adhoc-output document (if (nil? engine)
+                                         "combine5"
+                                         engine)))
+
+  (POST "/adhoc/process/" [document engine]
+    (.println System/out (format "document: %s, engine: %s" document engine))
+    (fn [req]
+      (view-adhoc-output req document engine (process/process
+                                              (if (nil? engine)
+                                                "combine5"
+                                                engine)
+                                              document))))
+
+    (resources "/")
+ )
+
+(def app
+  (-> viewer-routes
+      wrap-nested-params
+      wrap-keyword-params
+      wrap-params 
+      ))
