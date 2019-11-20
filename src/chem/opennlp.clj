8de5a337e42fbce9ac6dc9ca71a5d7827220f70c
Willie Rogers <wjrogers@nih.gov>
2019-11-20 16:29:08 -0500

    moved opennlp models from "models" to "data/models"

--- src/chem/opennlp.clj
+++ src/chem/opennlp.clj
@@ -2,7 +2,7 @@
   (:require [opennlp.nlp :as nlp]
             [clojure.string :as string]))
 
-(defonce get-sentences (nlp/make-sentence-detector "models/en-sent.bin"))
-(defonce tokenize      (nlp/make-tokenizer "models/en-token.bin"))
-(defonce pos-tag       (nlp/make-pos-tagger "models/en-pos-maxent.bin"))
+(defonce get-sentences (nlp/make-sentence-detector "data/models/en-sent.bin"))
+(defonce tokenize      (nlp/make-tokenizer "data/models/en-token.bin"))
+(defonce pos-tag       (nlp/make-pos-tagger "data/models/en-pos-maxent.bin"))
 
