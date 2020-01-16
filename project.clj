68e3d256774d99a152efbb5aa8745b393b452d28
Willie Rogers <wjrogers@nih.gov>
2020-01-16 17:10:21 -0500

    removed or commented out unused libraries from project file.

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                  [org.clojure/math.numeric-tower "0.0.2"]
                  [cc.mallet/mallet "2.0.7"]
                  [clojure-opennlp "0.3.2"] ;; uses Opennlp 1.5.3
-                 [se.sics/prologbeans "4.3.2"]
+                 ;; [se.sics/prologbeans "4.3.2"]
                  [org.apache.lucene/lucene-core "4.10.0"]
                  [org.apache.lucene/lucene-queryparser "4.10.0"]
                  [org.apache.lucene/lucene-queries "4.10.0"]
@@ -33,6 +33,9 @@
                  [uk.ac.cam.ch.opsin/opsin-core "2.4.0"]
                  [me.raynes/fs "1.4.6"]
                  [clj-glob "1.0.0"]
+                 ;; deeplearning library using Math Kernel Library (MKL)
+                 ;; be sure that The Intel Math Kernel Library is in LD_LIBRARY_PATH
+                 ;; [uncomplicate/neanderthal "0.24.0"]
                  [gov.nih.nlm.ctx/ctx "1.0-SNAPSHOT"]
                  [gov.nih.nlm.nlp/meta-stacking "1.0-SNAPSHOT"]
                  [gov.nih.nlm.nls/stanford-ner-extensions "1.0-SNAPSHOT"]
@@ -40,18 +43,15 @@
                  [skr "0.1.2-SNAPSHOT"]
                  [ii/utils "0.1.1-SNAPSHOT"]
                  [bioc "1.0.1"]
-                 [gov.nih.nlm.nls/metamap-api "2.0"]
                  ;; [metamap-api "2.0"]
                  ;; [gov.nih.nlm.nls/mallet-example "1.0-SNAPSHOT"]
                  [gov.nih.nlm.nls.lexaccess/lexaccess-dist "2013"]
                  [javax.servlet/servlet-api "2.5"]
                  [clj-diff "1.0.0-SNAPSHOT"]
                  [irutils "2.0"]
-                 [dosage-info "0.1.0-SNAPSHOT"]
                  [standoff "0.1.0-SNAPSHOT"]
-                 [com.novemberain/monger "3.1.0"]
-                 ;; [uncomplicate/neanderthal "0.24.0"]  ; be sure that The Intel Math Kernel Library is in LD_LIBRARY_PATH
-                 [congomongo "1.1.0"]]
+                 
+                 ]
   :plugins [[lein-ring "0.8.12" :exclusions [org.clojure/clojure]]]
   :repositories [["metamap" "https://metamap.nlm.nih.gov/maven2"]
                  ;; ["java.net" "https://download.java.net/maven/2"]
