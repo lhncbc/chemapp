d1bc583d69a84e11442cdc1ca757a8a42dfd1f7b
Willie Rogers <wjrogers@nih.gov>
2020-01-21 17:33:49 -0500

    removed mongodb references from chem.lookup.

--- src/chem/lookup.clj
+++ src/chem/lookup.clj
@@ -1,12 +1,12 @@
 (ns chem.lookup
-  (:require [chem.mongodb :as mongodb])
+  (:require [chem.irutils-normchem :as normchem])
   (:require [chem.pubchem :as pubchem])
   (:gen-class))
 
 ;; first look up chemical in database, if not found try PUBCHEM.
 
 (defn lookup [query]
-  (let [local-result (mongodb/lookup  :normchem query)]
+  (let [local-result (normchem/lookup  query)]
     (if (nil? local-result)
       (pubchem/lookup query)
       local-result)))
